7bb51de157dfa47f2cd8122a82a441fc8c54e663
dkmqflx <dkmqflx@gmail.com>
2026-02-10 16:41:13 +0900

    2. pydantic

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import os
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
 
+# 영화 정보를 위한 Pydantic 모델 선언
+from pydantic import BaseModel, Field
+
+class Movie(BaseModel):
+  title: str = Field(..., description="영화 제목")
+  director: str = Field(..., description="감독")
+  year: int = Field(..., description="개봉 연도")
+  genre: str = Field(..., description="장르")
+
 # .env 파일에서 환경 변수 로드
 load_dotenv()
 
@@ -20,5 +29,8 @@ model = init_chat_model("gemini-2.0-flash",
   model_provider="google_genai",
   api_key=os.getenv("GEMINI_API_KEY"))
 
-response = model.invoke("Hello, how are you?")
-print(response)
+# Movie 구조로 출력하도록 모델 래핑
+model = model.with_structured_output(Movie)
+
+response = model.invoke("Explain about the movie Truman Show")
+print(response) # title='The Truman Show' director='Peter Weir' year=1998 genre='Psychological comedy-drama'
